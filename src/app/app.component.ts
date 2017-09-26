0e5449ec0cfdec89097452d5d19539f5cbe3492f
Chebrolu, Azeet <Azeet.Chebrolu@aa.com>
2017-09-26 12:50:05 -0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Observable } from "rxjs/Rx";
-export interface ILookUp
-{
-  id:number;
-  description:string;
+import { Observable } from 'rxjs/Rx';
+import { CheckBoxListValidators } from './checkbox-list/checkbox-list.component';
+export interface ILookUp {
+  id: number;
+  description: string;
 }
 @Component({
   selector: 'app-root',
@@ -13,16 +13,17 @@ export interface ILookUp
 })
 export class AppComponent {
   title = 'app works!';
-  public selected:Array<ILookUp> = [{id:2,description:'Second'}];
+  // public selected:Array<ILookUp> = [{id:2,description:'Second'}];
+  public selected: Array<number> = [2];
   public reactiveForm: FormGroup;
-  data$:Observable<Array<ILookUp>> =Observable.of([{id:1,description:'First'},
-  {id:2,description:'Second'},{id:3,description:'Third'}
-  ,{id:4,description:'Fourth'},{id:5,description:'Fifth'}]);
-  constructor(private fb: FormBuilder){
-    
-    
+  data$: Observable<Array<ILookUp>> = Observable.of([{ id: 1, description: 'First' },
+  { id: 2, description: 'Second' }, { id: 3, description: 'Third' }
+    , { id: 4, description: 'Fourth' }, { id: 5, description: 'Fifth' }]);
+  constructor(private fb: FormBuilder) {
     this.reactiveForm = this.fb.group({
-      selected: [this.selected]
-    })
+      checkboxList: [null, [CheckBoxListValidators.required,
+      CheckBoxListValidators.minLength(1),
+      CheckBoxListValidators.maxLength(3)]]
+    });
   }
 }
--- src/app/checkbox-list/checkbox-list.component.ts
+++ src/app/checkbox-list/checkbox-list.component.ts
@@ -1,29 +1,32 @@
 
 import { Component, Input, forwardRef, ElementRef, Renderer, OnInit } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, FormControl, Validator } from '@angular/forms';
-let counter=0;
+import {
+  ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS,
+  FormControl, Validator, ValidatorFn, AbstractControl
+} from '@angular/forms';
+let counter = 0;
 export class CheckBoxListValidators {
-static required(): ValidatorFn {
+  static required(): ValidatorFn {
     return (c: AbstractControl): { [key: string]: boolean } | null => {
-      if ((<Array<any>>c.value).length === 0) {
+      if (c && (<Array<any>>c.value).length === 0) {
         return { 'required': true };
       }
       return null;
     };
   }
-static minLength(count: number): ValidatorFn {
+  static minLength(count: number): ValidatorFn {
     return (c: AbstractControl): { [key: string]: boolean } | null => {
-const selectedOptions = <Array<any>>c.value;
-      if (selectedOptions.length == count && count > 0) {
+      const selectedOptions = <Array<any>>c.value;
+      if (selectedOptions && selectedOptions.length < count && count > 0) {
         return { 'minlength': true };
       }
       return null;
     };
   }
-static maxLength(count: number): ValidatorFn {
+  static maxLength(count: number): ValidatorFn {
     return (c: AbstractControl): { [key: string]: boolean } | null => {
-      const selectedOptions = (<Array<any>>c.value;
-      if (selectedOptions.length > count && count > 0) {
+      const selectedOptions = <Array<any>>c.value;
+      if (selectedOptions && selectedOptions.length > count && count > 0) {
         return { 'maxlength': true };
       }
       return null;
@@ -31,103 +34,116 @@ static maxLength(count: number): ValidatorFn {
   }
 }
 @Component({
-    selector: 'ng-checkbox-list',
-    templateUrl: './checkbox-list.component.html',
-    styleUrls: ['./checkbox-list.component.css'],
-    providers: [
+  selector: 'ng-checkbox-list',
+  templateUrl: './checkbox-list.component.html',
+  styleUrls: ['./checkbox-list.component.css'],
+  providers: [
     {
       provide: NG_VALUE_ACCESSOR,
       useExisting: forwardRef(() => CheckboxListComponent),
       multi: true,
-    },
-    {
-      provide: NG_VALIDATORS,
-      useExisting: forwardRef(() => CheckboxListComponent),
-      multi: true,
-    }]
+    }
+    // ,{
+    //   provide: NG_VALIDATORS,
+    //   useExisting: forwardRef(() => CheckboxListComponent),
+    //   multi: true,
+    // }
+  ]
 })
-export class CheckboxListComponent implements ControlValueAccessor, Validator, OnInit {
-    @Input()source:Array<any>;
-    @Input() valueField:string;
-    @Input() labelField:string;
-    @Input() required:false;
-    @Input() minLength=0;
-    @Input() maxLength=0;
-    @Input() colClass='col-sm-4';
-    @Input('tabindex') _tabindex=0;
-    public identifier = `checkbox-${counter++}`;
+export class CheckboxListComponent implements
+  ControlValueAccessor
+  // , Validator
+  , OnInit {
+  @Input() source: Array<any>;
+  @Input() valueField: string;
+  @Input() labelField: string;
+  @Input() required: false;
+  @Input() returnValuesOnly: true; // value will be set to integer values of selected items
+  @Input() minLength = 0;
+  @Input() maxLength = 0;
+  @Input() colClass = 'col-sm-4';
+  @Input('tabindex') _tabindex = 0;
+  public identifier = `checkbox-${counter++}`;
+  private data: Array<any> = [];
+  constructor(private el: ElementRef, private renderer: Renderer) {
 
-    constructor(private el:ElementRef,private renderer: Renderer){
+  }
 
-    }
+  ngOnInit(): void {
+    // to remove the blue border around the control on tab
+    this.renderer.setElementAttribute(this.el.nativeElement, 'tabindex', null);
+  }
 
-    ngOnInit():void{
-      //to remove the blue border around the control on tab
-      this.renderer.setElementAttribute(this.el.nativeElement,"tabindex",null);
-    }
-
-    private data: Array<any>;
-
-    // this is the initial value set to the component
-    public writeValue(obj: any) {
-        if (obj) {
-            this.data = obj;
+  // this is the initial value set to the component
+  // can be an array of objects or an array of integer values
+  public writeValue(obj: Array<any>) {
+    if (obj) {
+      this.data = obj.map(o => {
+        if (o[this.valueField]) {
+          return o;
         }
+        const dummy = {};
+        dummy[this.valueField] = o;
+        return dummy;
+      });
+    } else {
+      this.data = [];
     }
-    public isChecked(item:any){
-      return this.data.find(d=>d[this.valueField]==item[this.valueField])!=null;
+  }
+  public isChecked(item: any) {
+    return this.data && this.data.find(d => d[this.valueField] === item[this.valueField]) != null;
+  }
+  // registers 'fn' that will be fired wheb changes are made
+  // this is how we emit the changes back to the form
+  public registerOnChange(fn: any) {
+    this.propagateChange = fn;
+  }
+
+  // validates the form, returns null when valid else the validation object
+  // UPDATE: use only custom validators
+  // public validate(c: FormControl) {
+  //   if (this.data.length === 0 && this.required) {
+  //     return { required: { valid: false } };
+  //   }
+  //   if (this.data.length === 0 && this.minLength > 0) {
+  //     return { minlength: { valid: false } };
+  //   }
+  //   if (this.data.length > this.maxLength && this.maxLength > 0) {
+  //     return { maxlength: { valid: false } };
+  //   }
+
+  //   return null;
+
+  // }
+
+  // not used, used for touch input
+  public registerOnTouched(fn: any) {
+    this.propagateTouch = fn;
+  }
+  private onTouch() {
+    this.propagateTouch();
+  }
+  // change events from the textarea
+  private onChange(event) {
+
+    // get value from text area
+    const newValue: number = parseInt(event.target.value, 10);
+    const checked = event.target.checked;
+    const item: any = this.source.find(d => d[this.valueField] === newValue);
+    const existing = this.data.find(d => d[this.valueField] === newValue);
+
+
+    if (existing) {
+      const idx = this.data.findIndex(d => d[this.valueField] === newValue);
+      this.data.splice(idx, 1);
+    } else {
+      this.data.push(item);
     }
-    // registers 'fn' that will be fired wheb changes are made
-    // this is how we emit the changes back to the form
-    public registerOnChange(fn: any) {
-        this.propagateChange = fn;
-    }
+    // update the form.the control value will be an array of objects selected or an array of integers
+    this.propagateChange(this.data.map(d => this.returnValuesOnly ? d[this.valueField] : d));
+  }
 
-    // validates the form, returns null when valid else the validation object
-    public validate(c: FormControl) {
-      if(this.data.length==0 && this.required){
-        return {required:{valid:false}};
-      }
-      if(this.data.length==0 && this.minLength>0){
-        return {minlength:{valid:false}};
-      }
-      if(this.data.length>this.maxLength && this.maxLength>0){
-        return {maxlength:{valid:false}};
-      }
-
-      return null;
-
-    }
-
-    // not used, used for touch input
-    public registerOnTouched(fn: any) {
-      this.propagateTouch = fn;
-    }
-    private onTouch(){
-      this.propagateTouch();
-    }
-    // change events from the textarea
-    private onChange(event) {
-
-        // get value from text area
-        let newValue:any = event.target.value;
-        let checked = event.target.checked;
-        let item:any = this.source.find(d=>d[this.valueField]==newValue);
-        let existing = this.data.find(d=>d[this.valueField]==newValue);
-
-
-        if(existing){
-          let idx = this.data.findIndex(d=>d[this.valueField]==newValue);
-          this.data.splice(idx,1);
-        }
-        else{
-          this.data.push(item);
-        }
-        // update the form
-        this.propagateChange(this.data);
-    }
-
-    // the method set in registerOnChange to emit changes back to the form
-    private propagateChange = (_: any) => { };
-    private propagateTouch = () => {};
+  // the method set in registerOnChange to emit changes back to the form
+  private propagateChange = (_: any) => { };
+  private propagateTouch = () => { };
 }
